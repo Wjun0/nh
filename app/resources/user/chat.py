9b6384df053f9c3cb4682f8a507d194a831f3c8b
wangjun <2665254503@qq.com>
2020-01-14 15:55:00 +0800

    历史记录查询，时间转换

--- app/resources/user/chat.py
+++ app/resources/user/chat.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import time
 
@@ -13,6 +14,70 @@ from app import redis_client
 from utils.TfServer import TFserver
 
 
+
+def save_history_to_mysql(session_id,result,question):
+    data = HistoryDialogue(
+        time=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()),
+        session_id=session_id,
+        username=session_id,
+        data=json.dumps(result),
+        question=question
+    )
+    try:
+        db.session.add(data)
+        db.session.commit()
+
+    except Exception as e:
+        current_app.logger.error(e)
+
+        db.session.rollback()
+        return {'message': "error",
+                "data": "请求失败"}, 400
+
+
+def save_result_to_redis(result,session_id):
+    try:
+        json_str_data = json.dumps(result)
+        redis_client.hset(session_id,session_id,json_str_data)
+    except Exception as e:
+        current_app.logger.error(e)
+
+        return {'message': "error",
+                "data": "请求失败"}, 400
+
+
+def handle_time(start,end):
+    if ':' in start:
+        try:
+            start_time = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+        except Exception as e:
+            current_app.logger.error(e)
+            return None,None
+    else:
+        try:
+            start_time = datetime.datetime.strptime(start, '%Y-%m-%d')
+        except Exception as e:
+            current_app.logger.error(e)
+            return None,None
+
+    if ':'in end:
+        try:
+            end_time = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
+        except Exception as e:
+            current_app.logger.error(e)
+            return None,None
+    else:
+        try:
+            end = datetime.datetime.strptime(end, '%Y-%m-%d')
+            end_time = end + datetime.timedelta(days=1)
+        except Exception as e:
+            current_app.logger.error(e)
+            return None,None
+
+    return start_time,end_time
+
+
+
 class ChatResource(Resource):
     '''聊天'''
     def post(self):
@@ -25,7 +90,6 @@ class ChatResource(Resource):
         session_id = args.session_id
 
         if session_id == "Round0":
-            # session_id等于0 新建会话
 
             # 获取算法数据
             query = {"query": question,
@@ -36,64 +100,37 @@ class ChatResource(Resource):
             result = TF.get_result(query)
 
             #将数据保存到redis
-            # json_str_data = json.dumps(data_dict)
-            # redis_client.hset('admin02','R1',json_str_data)
+            save_result_to_redis(result,session_id)
 
             # 将数据保存到mysql
-
-            #生成日志
-            # current_app.logger.error('errormessage')
+            save_history_to_mysql(session_id,result,question)
 
             return result
 
         else:
             # 否则基于session_id查询
+
+            # 向redis 取出相应的问题和session_id,调用算法获取数据
+            redis_data = redis_client.hget(session_id,session_id)
+
             query = {"query": question,
                      "sessID": session_id,
-                     "table": None}
+                     "table": redis_data}
 
             TF = TFserver()
             result = TF.get_result(query)
 
+            session_id = result.get('sessID')
+
+
+            #将查询结果保存到redis
+            save_result_to_redis(result,session_id)
+
+            #将查询结果保存到mysql
+            save_history_to_mysql(session_id, result, question)
+
             return result
 
-        # his = HistoryDialogue(
-        #     time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()),
-        #     session_id = "zs_1",
-        #     username= "张三",
-        #     data = {"username":"shangdan","age":"23"},
-        #     question = question
-        # )
-        # try:
-        #     db.session.add(his)
-        #     db.session.commit()
-        # except:
-        #     db.session.rollback()
-        #     return {'message':"error",
-        #             "data":"添加数据失败"},400
-
-        # 根据前端的数据调用相应的算法获取数据
-
-        datas = db.session.execute("select * from historical_dialogue_table order by id desc").fetchone()
-        result = {
-            "id":datas.id,
-            "time":str(datas.time),
-            "session_id":datas.session_id,
-            "username":datas.username,
-            "data":datas.data,
-            "question":datas.question
-        }
-
-        return result
-
-
-    # 测试redis
-    def get(self):
-        result = redis_client.hget('admin02','R1')
-
-        print(json.loads(result).get('question'))
-
-        return {"data":result}
 
 
 class HistoryResource(Resource):
@@ -103,23 +140,28 @@ class HistoryResource(Resource):
         parser.add_argument('end',required=True,location='json')
         args = parser.parse_args()
 
-        start_time = args.start
-        end_time = args.end
+        start = args.start
+        end = args.end
+
+        #时间处理
+        start_time,end_time = handle_time(start,end)
+
+        if start_time is None or end_time is None:
+            return {'message':'error',"data":"时间格式错误"}
 
         result = db.session.query(HistoryDialogue).filter(HistoryDialogue.time.between(start_time,end_time))
 
-        data = {}
+        data_list = []
         for i in result:
             dict = {
                 'time':str(i.time),
                 'username':i.username,
-                'data':i.data,
+                'data':json.loads(i.data),
                 'question':i.question,
-                'session_id':i.session_id
             }
-            data[i.id] = dict
+            data_list.append(dict)
 
-        return data
+        return data_list
 
 
 
--- app/resources/user/test.py
+++ app/resources/user/test.py
@@ -1,7 +1,9 @@
+import json
 
 from flask import jsonify
 from werkzeug.security import check_password_hash
 
+from app import redis_client
 from . import user_chat
 
 
@@ -9,5 +11,19 @@ from . import user_chat
 def my_test():
     print('ok')
     dict = {"name":"张三"}
+
+    redis_client.expire('Round2',1)  #设置0.01秒过期
     return jsonify(dict)
 
+
+# 测试redis
+@user_chat.route('/test/redis',methods=['get'])
+def redis_test():
+    result = redis_client.hget('admin02','R1')
+
+    redis_client.hset('Round2','test1','{name:test1}')
+    redis_client.hset('Round2','test2','{name:test2}')
+
+    # print(json.loads(result))
+
+    return jsonify({"message":'ok'})
